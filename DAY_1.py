22e88d46d75bb9b7bdba10bb24b9dc15de9c283f
yuvanikaac <156583870+yuvanikaac@users.noreply.github.com>
2025-12-25 17:24:49 +0530

    Add files via upload

---
+++ DAY_1.py
@@ -0,0 +1,62 @@
+# Q1 Create a variable age and store the value 21. Print it.
+age = 21
+print(f"The age is {age}")
+# Q2 Create two variables a = 10 and b = 5. Print their sum.
+a = 10
+b= 5
+sum= a+b
+print(f"the sum is {sum}")
+# Q3 Create a variable college and assign your college name as a string. Print it.
+college = "MEENAKSHI SUNDARARAJAN ENGINEERING COLLEGE"
+print(f"I am from {college}")
+# Q4 Create a string variable name with value "Python". Print the string.
+name = "python"
+print(name)
+'''Q5 Create two string variables:
+•	first = "Hello"
+•	second = "World"
+Print them together with a space in between.'''
+first = "Hello"
+second = "World"
+print(f"{first} {second}")
+
+
+# Q5 Create a boolean variable is_python_easy and assign it True. Print it.
+is_python_easy = True
+if is_python_easy:
+    print("easy")
+else:
+    print("not easy")
+
+
+# Q11 Create a float variable price with value 99.99. Print it.
+price = 99.99
+print(f"The price of rice is {price}")
+
+# Q12 Create two float values a = 10.5 and b = 2.5. Print their sum.
+a = 10.5
+b = 2.5
+sum =a+b
+print(f"the sum of {a} and {b} is {sum}")
+
+'''Q13 Create:
+•	name = "Alice"
+•	age = 20
+Print the sentence:
+Alice is 20 years old'''
+name = "Alice"
+age = 20
+print(f"{name} is {age} years old")
+
+
+
+'''Q14 Create a variable is_raining = False.
+Print "Take umbrella" only using the variable value (no direct printing).'''
+is_raining = False
+if is_raining:
+    print("No need  to take umberella")
+else:
+    print(" take umbrella")
+
+
+
