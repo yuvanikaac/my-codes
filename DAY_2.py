1146aa000c9ab85d8ae9b86e776dffc50fe56552
yuvanikaac <156583870+yuvanikaac@users.noreply.github.com>
2025-12-26 18:18:58 +0530

    Add files via upload

---
+++ DAY_2.py
@@ -0,0 +1,131 @@
+
+#Q1 Convert the string `"25"` into an integer and print it.
+s = "25"
+s = int(s)
+print("The string is typecasted tointeger",s)
+print("The type of the value after typecasting is:",type(s))
+
+# Q2 Convert the integer `10` into a float and print it.
+n = 10
+n = float(n)
+print("The integer number is typecasted to float",n)
+print("The type of the value after typecasting is:",type(n))
+
+# Q3 Convert the float `7.9` into an integer and print it.
+f = 7.9
+f = int(f)
+print("the float number is typecasted to integer",f)
+print("The type of the value after typecasting is:",type(f))
+
+# Q4 Convert the boolean `True` into an integer and print it.
+is_value = True
+is_value = int(is_value)
+print("The boolean true value is typecasted into integer",is_value)
+print("The type of the value after typecasting is:",type(is_value))
+
+# Q5 Convert the boolean `False` into an integer and print it.
+is_wrong = False
+is_wrong = int(is_wrong)
+print("The boolean  false value after typecasting to integer",is_wrong)
+print("The type of the value after typecasting is:",type(is_wrong))
+
+# Q6 Convert the string `"3.14"` into a float and print it.
+a = "3.14"
+a = float(a)
+print("The string value after typecasting into float",a)
+print("The type of the value after typecasting is:",type(a))
+
+
+# Q7 Convert the integer `50` into a string and print it.
+i = 50
+i = str(i)
+print("The integer value after typecasting into string",i)
+print("The type of the value after typecasting is:",type(i))
+
+# Q8 Convert the float `99.5` into a string and print it.
+v = 99.5
+v = str(v)
+print("The float value after typecasting into string",v)
+print("The type of the value after typecasting is:",type(v))
+
+
+# Q9 Convert the string `"100"` into an integer, add `20`, and print the result.
+g = "100"
+g = int(g)
+b = g+20
+print("The value after adding 20 is",b)
+
+# Q10 Convert the string `"45.6"` into a float, add `4.4`, and print the result.
+f = "45.6"
+f = float(f)
+e = f+20
+print("The value after adding 20 is",e)
+
+# Q11 Convert the integer `1` into a boolean and print it.
+x = 1
+x = bool(x)
+print("The integer value after typecasting into boolean",x)
+print("The type of the value after typecasting is:",type(x))
+
+# Q12 Convert the integer `0` into a boolean and print it.
+y = 0
+y = bool(y)
+print("The integer value after typecasting into boolean",y)
+print("The type of the value after typecasting is:",type(y))
+
+
+# Q13 Convert the string `"Hello"` into a boolean and print it.
+z = "Hello"
+z = bool(z)
+print("The string value after typecasting into boolean",z)
+print("The type of the value after typecasting is:",type(z))
+
+
+#Q14 Convert the empty string `""` into a boolean and print it.
+e = ""
+e = bool(e)
+print("The  empty string value after typecasting into boolean",e)
+print("The type of the value after typecasting is:",type(e))
+
+# Q15 Convert the string `"0"` into a boolean and print it.
+d = "0"
+d = bool(d)
+print("The  0 string value after typecasting into boolean",d)
+print("The type of the value after typecasting is:",type(d))
+
+
+# Q16 Convert the float `0.0` into a boolean and print it.
+h = 0.0
+h = bool(h)
+print("The  float value after typecasting into boolean",e)
+print("The type of the value after typecasting is:",type(h))
+
+# Q17 Convert the integer `-10` into a boolean and print it.
+n = -10
+n = bool(n)
+print("The  negative integer  value after typecasting into boolean",n)
+print("The type of the value after typecasting is:",type(n))
+
+# Q18 Convert the boolean `True` into a float and print it.
+t = True
+t = float(t)
+print("The  true boolean value after typecasting into float",t)
+print("The type of the value after typecasting is:",type(t))
+
+# Q19 Convert the boolean `False` into a string and print it.
+f = False
+f = float(f)
+print("The  true boolean value after typecasting into float",f)
+print("The type of the value after typecasting is:",type(f))
+
+#Q20 Convert the string `"10"` to an integer, the integer to a float, and print the final value.
+print("Now we are converting the string into an integer")
+string = "10"
+string = int(string)
+print("The type of the value after typecasting is:",type(f))
+print("Now we are going to convert the integer value into float value")
+string = float(string)
+print("The type of the value after typecasting is:",type(f))
+
+
+
